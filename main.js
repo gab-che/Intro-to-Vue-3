9a0250bec8d09e4482cf594d9877558c32ac1f36
gab-che <gabriellachessa1@gmail.com>
2023-03-24 22:28:48 +0100

    L5

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {
+                    id: 2324,
+                    color: 'blue'
+                },
+                {
+                    id: 3421,
+                    color: 'green'
+                }
+            ],
+            sizes: ['XS', 'S', 'M', 'L', 'XL']
         }
     }
 })
